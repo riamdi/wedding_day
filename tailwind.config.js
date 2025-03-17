8445c831f13a97aaa515a90ba84e2ecd2cbf3fa5
riamdi <shihanova.ek@gmail.com>
2025-03-17 14:49:36 +0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        bg_color: '#252525'
+      },
+      fontFamily: {
+        noto: ["Noto Serif Traditional Chinese", "serif"],
+        Bonche: ["	Bonche"],
+      },
+    },
+  },
+  plugins: [],
+}
+
