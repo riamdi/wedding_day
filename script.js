14778a79eac47d01883b4a6e4208345c20fd2f2d
riamdi <shihanova.ek@gmail.com>
2025-04-03 01:13:18 +0400

    add:modal

--- script.js
+++ script.js
@@ -37,4 +37,68 @@ document.addEventListener("DOMContentLoaded", function () {
     updateCountdown();
     setInterval(updateCountdown, 1000);
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const submitBtn = document.getElementById("submit-btn");
+    const modal_section = document.getElementById("modal_section");
+    const closeBtn = document.getElementById("close-modal");
+    const nameInput = document.getElementById("nameInput");
+    const errorTooltip = document.getElementById("error_tooltip");
+    const errorModal = document.getElementById("error_modal");
+    const closeErrorBtn = document.getElementById("close-error-modal");
+
+    function fadeIn(element) {
+        element.classList.remove("hidden");
+        element.classList.add("opacity-0");
+        void element.offsetWidth;
+        element.classList.remove("opacity-0");
+        element.classList.add("opacity-100");
+    }
+    function fadeOut(element, callback) {
+        element.classList.remove("opacity-100");
+        element.classList.add("opacity-0");
+        
+        setTimeout(() => {
+            element.classList.add("hidden");
+            if (callback) callback();
+        }, 1000); 
+    }
+
+    submitBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+        const inputValue = nameInput.value.trim();
+
+        // Если поле ПУСТОЕ
+        if (inputValue === "") {
+            fadeIn(errorTooltip);
+            fadeIn(errorModal);
+            
+            nameInput.classList.remove("border-b-[#000]", "border-b");
+            nameInput.classList.add("border-2", "border-red-500");
+
+            setTimeout(() => fadeOut(errorTooltip), 10000);
+            setTimeout(() => fadeOut(errorModal), 10000);
+        } 
+        else {
+            if (!errorTooltip.classList.contains("hidden")) fadeOut(errorTooltip);
+            if (!errorModal.classList.contains("hidden")) fadeOut(errorModal);
+            
+            nameInput.classList.remove("border-red-500", "border-2");
+            nameInput.classList.add("border-b-[#000]", "border-b");
+            
+            fadeIn(modal_section);
+        }
+    });
+
+    closeBtn.addEventListener("click", () => fadeOut(modal_section));
+    closeErrorBtn.addEventListener("click", () => fadeOut(errorModal));
+    
+    modal_section.addEventListener("click", (e) => {
+        if (e.target.classList.contains("fixed")) fadeOut(modal_section);
+    });
+    
+    errorModal.addEventListener("click", (e) => {
+        if (e.target.classList.contains("fixed")) fadeOut(errorModal);
+    });
+});
+
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,14 @@ module.exports = {
     "./css/**/*.css",
     "./js/**/*.js",
     "./img/**/*.{jpg,png,svg}"
+    // "./node_modules/**/*.css"
   ],
   theme: {
     extend: {
       colors: {
         bg_color: '#252525',
-        black: '#000000'
+        black: '#000000',
+        red_btn: '#f95d51'
       },
       fontFamily: {
         'noto': ["Noto Serif Traditional Chinese", "serif"],
@@ -18,6 +20,9 @@ module.exports = {
         'mak' : ["Mak"],
         'cezanne' : ['Cezanne']
       },
+      boxShadow: {
+        '3xl': '0 0 7px 7px rgba(0, 0, 0, 0.08)',
+      }
     },
   },
   plugins: [],
